b9c47431d78c0e3995205ea635b32a96ee5d7d41
z347 <shulo.vova@gmail.com>
2021-02-10 03:32:14 +0200

    deleted useless folder with files

--- old/dist/middlewares/invalid-request-handler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const invalidRequestHandler = (req, res) => res.status(404).json({ message: 'Not valid request' });
-exports.default = invalidRequestHandler;
-//# sourceMappingURL=invalid-request-handler.js.map
